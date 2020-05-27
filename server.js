6523b7e33c6c3889a7745180beebfec934310eab
AmmonMatthews <ammonmatthews@gmail.com>
2020-05-27 12:35:08 -0600

    heroku deployment

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ server.get("/", (req, res, next) => {
         message: "Welcome to VR Direct!"
     })
 })
-server.use((err, req, res, next) => {
+server.use((err, message, stack, req, res, next) => {
     console.log("error", err)
     return res.status(500).json ({ 
-        message: "uh oh there was a problem!"
+        message, err, stack
     })
 })
 
