5190c91c20af367bc4d8396463e0507adb0238cc
unknown <C.Berumen51@gmail.com>
2020-05-26 18:00:10 -0700

    modified knexfile

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,16 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    migrations: {directory: './data/migrations'},
-    seeds: {directory: "./data/seeds",},
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
   }
 };
