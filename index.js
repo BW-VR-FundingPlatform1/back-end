8861f16654b32cf5038f76a3c8fa06e6f8e16b5a
unknown <C.Berumen51@gmail.com>
2020-05-26 17:13:14 -0700

    changed port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server');
 
-const PORT = process.env.PORT || 4900;
-server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+const port = process.env.PORT || 4900;
+server.listen(port, () => {
+  console.log(`\n=== Server listening on port ${port} ===\n`);
 });
