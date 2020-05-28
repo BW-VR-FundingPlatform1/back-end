4574725dcd176358f05dcfad0fbf0c6a3e5f82b6
unknown <C.Berumen51@gmail.com>
2020-05-27 17:26:54 -0700

    modified env

--- data/migrations/20200521232118_entrepreneur.js
+++ data/migrations/20200521232118_entrepreneur.js
@@ -10,9 +10,33 @@ exports.up = async function(knex) {
         table.string("phone").notNull()
         table.string("email", 280).notNull()
     })
+
+    await knex.schema.createTable("backer", (table) => {
+      table.increments("id")
+      table.string("username", 280).notNull().unique()
+      table.string("password", 280).notNull()
+      table.string("FirstName", 280).notNull()
+      table.string("LastName", 280).notNull()
+      table.string("address", 280).notNull()
+      table.string("phone").notNull()
+      table.string("email", 280).notNull()
+  })
+
+    await knex.schema.createTable("projects", (table) => {
+      table.increments("id")
+      table.string("companyName").notNull().unique()
+      table.string("projectName").notNull().unique()
+      table.string("fundingAmount").notNull().unique()
+
+
+
+
+    })
   }
   
   exports.down = async function(knex) {
     await knex.schema.dropTableIfExists("entrepreneur")
+    await knex.schema.dropTableIfExists("backer")
+
   }
   
